986470678cac0768a12e771e787d769aecd05130
Luis Fernando <fernando_sorocaba_telefonica@hotmail.com>
2025-05-18 13:49:55 -0300

    DEBUG False

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2by@5k=4eo(sifou3_z)%n8v(0_=ii4zo1y91dazh-^ro#oa@&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['agendavirtual.alwaysdata.net', '127.0.0.1', 'localhost']
 
